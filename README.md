ef0b980111ff3c4ee75b124ea34f011e3404ba8f
frmarfull <fr.marfull@alumnos.duoc.cl>
2020-12-20 18:29:32 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# CLI Ball Sort Puzzle Game
+[![Screenshot](./screenshot-cmder-and-python-idle.png)](https://github.com/Marfullsen/cli-ball-sort)
+[![GitHub version](https://img.shields.io/badge/Commits-1-red.svg)](https://github.com/Marfullsen/cli-ball-sort)
+[![GitHub version](https://img.shields.io/badge/Version-1.0-green.svg)](https://github.com/Marfullsen/cli-ball-sort)
+[![GitHub version](https://img.shields.io/badge/Python-3.7-blue.svg)](https://github.com/Marfullsen/cli-ball-sort)
+
+---
+
+## Desc
+Game like "Ball Sort Puzzle" written in Python for the command-line.
+---
+
+## Usage
+1. Install `git` & `python3`.
+2. Clone and enter the directory.
+``` 
+git clone https://github.com/Marfullsen/cli-ball-sort.git
+cd cli-ball-sort
+python cli-ball-sort.py
+```
+3. Have fun!
+
+---
+
+## How to play
+
+1) Place elements with same char into one row.
+2) Take any element at the end of a row and move it into an empty row or next to an element with same char.
+3) You win if all rows are filled with only one type of element, or are empty.
+Viel Glück! :four_leaf_clover:
+
+### beginner game: 
+#### 4 rows & 1 Empty. (very easy).
+
+### Standard game:
+#### 5 rows & 2 empty. (easy).
+
+### Moderate game:
+#### 6 rows & 2 empty (Medium).
+
+### Difficult game:
+#### 8 rows & 2 empty (Hard).
+
+## Madness game:
+### 11 rows & 2 empty (Extreme).
+---
+
+## Credit
+This game is inspired by [Ball Sort Puzzle](https://play.google.com/store/apps/details?id=com.GMA.Ball.Sort.Puzzle).
+Other games like this are:
+[Sort Puzzle: Fun Ball](https://play.google.com/store/apps/details/Sort_Puzzle_Fun_Ball?id=com.playcus.ball.sort.puzzle)
+[Ball Sort Max](https://play.google.com/store/apps/details?id=com.bsm.ballsortmax)
+[Sort It 3D](https://play.google.com/store/apps/details?id=com.game.sortit3d)
+[Water Sort Puzzle](https://play.google.com/store/apps/details?id=com.gma.water.sort.puzzle)
+[Liquid Sort Puzzle - Water Sort Puzzle](https://play.google.com/store/apps/details?id=com.picolaf.liquidsortpuzzle)
---
+++ cli-ball-sort.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from random import shuffle as revolver
+from os import system
+
+tubos = list()
+totalTubos = int(input('Ingrese cantidad total de tubos (5-12): '))
+vacios = int(input('Ingrese de tubos vacíos (1-2): ')) 
+if not (totalTubos - vacios > 2): totalTubos = 5; vacios = 2
+
+MAX_ITEMS = 4
+
+def display():
+    char = 65 # Para ver números: 48, para ver letras: 65.
+    for num, tubo in enumerate(tubos):
+        print(f'{num+1}) |==',end='')
+        for i, elemento in enumerate(tubo):
+            if i < len(tubo)-1: print(f' {chr(int(elemento)+char)} ',end='-')
+            else: print(f' {chr(int(elemento)+char)} ',end='')
+        print('')
+
+def mover(a,b):
+    a-=1; b-=1
+    if len(tubos[a]) > 0 and len(tubos[b]) < MAX_ITEMS: # Si no sacas de un tubo vacío ni mueves a un tubo lleno:
+        if len(tubos[b]) == 0: # Si mueves a un tubo vacío:
+            tubos[b].append(tubos[a].pop())
+        elif tubos[a][-1] == tubos[b][-1]: # Si mueves un elemento sobre otro igual:
+            tubos[b].append(tubos[a].pop())
+        else:
+            input('\n¡Sólo se puede mover sobre vacío o sobre el mismo ELEMENTO!\n')
+    else:
+        input('\n¡NO ES POSIBLE ESA JUGADA!\n'
+        'No puedes sacar de un tubo vacío, '
+        'tampoco mover a un tubo lleno.\n'
+        'Intenta nuevamente. '
+        f'Cantidad máxima de elementos por tubo: {MAX_ITEMS}\n'
+              )
+
+def nuevoJuego():
+    global tubos
+    cuartetosAlAzar = list(''.join([str(i)*MAX_ITEMS for i in range(totalTubos-vacios)]))
+    revolver(cuartetosAlAzar)
+    tubos = [cuartetosAlAzar[x*MAX_ITEMS:x*MAX_ITEMS+MAX_ITEMS] for x in range(totalTubos)]
+    
+    while verificar():
+        print('\n\nMmmm.... la baraja no se revolvió bien...\nREVOLVIENDO NUEVAMENTE!\n\n')
+        nuevoJuego()
+
+def verificar():
+    completado = True
+    for tubo in tubos:
+        if not (len(set(tubo)) < 2 and len(tubo) in [0, MAX_ITEMS]):
+            completado = False
+            break
+    return completado
+
+nuevoJuego()
+display()
+
+while True:
+    print('-------')
+    mover(
+        int( input('Ingrese N° de tubo para sacar el último número: ') ),
+        int( input('Ingrese N° de tubo agregar el número sacado número: ') )
+        )
+    system('cls')
+    display()
+    if verificar(): break
+input('¡Has ganado!')
